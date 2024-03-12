eb667468a712cfe8c55d30627cd8e1a0bd301056
Kevin Dev <kevin.developer71@gmail.com>
2024-03-12 12:44:43 -0600

    logs

--- src/app/otra/page.tsx
+++ src/app/otra/page.tsx
@@ -1,11 +1,11 @@
 const page = () => {
   return (
     <div className="h-screen flex flex-col gap-4">
-      <a className="text-blue-300" href="http://localhost:3000/otra?result=success">
+      <a className="text-blue-300" href="/otra?result=success">
         simular accion de exito
       </a>
       <br />
-      <a className="text-blue-300" href="http://localhost:3000/otra?result=fail">
+      <a className="text-blue-300" href="/otra?result=fail">
         simular accion de fail
       </a>
     </div>
